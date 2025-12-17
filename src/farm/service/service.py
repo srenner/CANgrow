ea8350c5095ea77d48b502346b07d5180f5497f1
Shawn Renner <shawn.renner@gmail.com>
2025-12-16 21:51:49 -0600

    basic farm service structure

---
+++ src/farm/service/service.py
@@ -0,0 +1,28 @@
+import can
+import time
+import os
+import queue
+from threading import Thread
+
+# setup gpio pins, etc
+
+def can_rx_task():
+    while True:
+        print("Rx:" + str(time.time()) + "\n")
+        time.sleep(1)
+
+def can_tx_task():
+    while True:
+        print("Tx:" + str(time.time()) + "\n")
+        time.sleep(1)
+
+q = queue.Queue()
+rx = Thread(target = can_rx_task)  
+rx.start()
+tx = Thread(target = can_tx_task)
+tx.start()
+
+while True:
+    time.sleep(1)
+    
+
