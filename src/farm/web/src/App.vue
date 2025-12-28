ba3271d8c49c5c1e366dd69e0e6d826d4fedce78
Shawn Renner <shawn.renner@gmail.com>
2025-12-27 18:43:01 -0600

    remove dead code

--- src/farm/web/src/App.vue
+++ src/farm/web/src/App.vue
@@ -1,31 +1,10 @@
 <script setup lang="ts">
-  import { ref, onMounted } from 'vue'
+  import { onMounted } from 'vue'
   import Header from './components/Header.vue';
 
-  const dark = ref<boolean>(false);
-
-  function  applyTheme(dark:boolean) {
-    const root = document.documentElement;
-    root.classList.toggle('dark', dark);
-  }
-
   onMounted(() => {
-    const saved = localStorage.getItem('theme');
-    if(saved) {
-      dark.value = saved === 'dark';
-    }
-    else {
-      dark.value = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    }
-    applyTheme(dark.value);
+    //
   });
-
-  function toggleTheme(): void {
-    dark.value = !dark.value;
-    applyTheme(dark.value);
-    localStorage.setItem('theme', dark.value ? 'dark': 'light');
-  }
-
 
 </script>
 
