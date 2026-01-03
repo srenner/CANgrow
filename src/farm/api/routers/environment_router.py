911d6515b3c9a063ec14350bfd8abbf808fc6331
Shawn Renner <shawn.renner@gmail.com>
2026-01-02 21:54:24 -0600

    mild session refactor, get environment by id

--- src/farm/api/routers/environment_router.py
+++ src/farm/api/routers/environment_router.py
@@ -1,5 +1,5 @@
 from typing import List
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from sqlmodel import Session, create_engine
 # from models import Environment, EnvironmentCreate, EnvironmentPatch, EnvironmentPublic
@@ -10,37 +10,48 @@ from models.environment import *
 settings = Settings()
 engine = create_engine(settings.DATABASE_URL, echo=True, connect_args=settings.CONNECT_ARGS)
 
+def get_session():
+    with Session(engine) as session:
+        yield session
+
 router = APIRouter(
     prefix="/environment",
     tags=["environment"]
 )
 
 @router.get("", response_model=List[EnvironmentPublic], operation_id="listEnvironments")
-def read_environments():
-    with Session(engine) as session:
-        environments = session.exec(select(Environment)
-                                    .where(Environment.is_active == True)).scalars().all()
-        return environments
-    
+def get_environments(*, session = Depends(get_session)):
+    environments = session.exec(select(Environment)
+                                .where(Environment.is_active == True)).scalars().all()
+    return environments
+
+@router.get("/{id}", response_model=EnvironmentPublic, operation_id="getEnvironment")
+def get_environment(*, session: Session = Depends(get_session), id: int, include_inactive: bool = False):
+    query = select(Environment).where(Environment.id == id)
+    if not include_inactive:
+        query = query.where(Environment.is_active == True)
+    environment = session.exec(query).scalar_one_or_none()
+    if environment is None:
+        raise HTTPException(status_code=404)
+    return environment
+
 @router.post("", response_model=EnvironmentPublic, operation_id="createEnvironment")
-def create_environment(environment: EnvironmentCreate):
-    with Session(engine) as session:
-        db_environment = Environment.model_validate(environment)
-        session.add(db_environment)
-        session.commit()
-        session.refresh(db_environment)
-        return db_environment
+def post_environment(*, session: Session = Depends(get_session), environment: EnvironmentCreate):
+    db_environment = Environment.model_validate(environment)
+    session.add(db_environment)
+    session.commit()
+    session.refresh(db_environment)
+    return db_environment
     
 @router.patch("/{id}", response_model=EnvironmentPublic, operation_id="updateEnvironment")
-def update_environment(id: int, environment: EnvironmentPatch):
-    with Session(engine) as session:
-        db_environment = session.get(Environment, id)
-        if not db_environment:
-            raise HTTPException(status_code=404, detail="Environment Not Found")
-        update_data = environment.model_dump(exclude_unset=True)
-        for field, value in update_data.items():
-            setattr(db_environment, field, value)
-        session.add(db_environment)
-        session.commit()
-        session.refresh(db_environment)
-        return db_environment
+def patch_environment(*, session: Session = Depends(get_session), id: int, environment: EnvironmentPatch):
+    db_environment = session.get(Environment, id)
+    if not db_environment:
+        raise HTTPException(status_code=404, detail="Environment Not Found")
+    update_data = environment.model_dump(exclude_unset=True)
+    for field, value in update_data.items():
+        setattr(db_environment, field, value)
+    session.add(db_environment)
+    session.commit()
+    session.refresh(db_environment)
+    return db_environment
