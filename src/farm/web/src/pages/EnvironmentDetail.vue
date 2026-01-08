88111da89821f97e3872d4f26d44ba44733dc5e4
Shawn Renner <shawn.renner@gmail.com>
2026-01-07 22:43:35 -0600

    load active profile

--- src/farm/web/src/pages/EnvironmentDetail.vue
+++ src/farm/web/src/pages/EnvironmentDetail.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
     import { useRoute } from 'vue-router';
     import { ref, onMounted, computed } from 'vue'
-    import type { EnvironmentPublic } from '@/api/types.gen';
-    import { Environment as EnvironmentService } from '@/api/sdk.gen';
-import EnvironmentHistoryLatest from '@/components/EnvironmentHistoryLatest.vue';
+    import { type EnvironmentProfilePublic, type EnvironmentPublic } from '@/api/types.gen';
+    import { Environment as EnvironmentService, EnvironmentProfile as EnvironmentProfileService } from '@/api/sdk.gen';
+    import EnvironmentHistoryLatest from '@/components/EnvironmentHistoryLatest.vue';
 
     const route = useRoute()
     const id = computed(() => parseInt(route.params.id as string))
 
     const environment = ref<EnvironmentPublic>();
+    const environmentProfile = ref<EnvironmentProfilePublic>();
     const loading = ref(false);
     const error = ref<string | null>(null);
 
@@ -21,7 +22,7 @@ import EnvironmentHistoryLatest from '@/components/EnvironmentHistoryLatest.vue'
         error.value = null;
         try {
             const { data } = await EnvironmentService.getEnvironment({ path: { id: id.value } });            
-            environment.value = data
+            environment.value = data;
         }
         catch(err: any) {
             error.value = err.message || 'Error loading environment ' + id
@@ -29,6 +30,22 @@ import EnvironmentHistoryLatest from '@/components/EnvironmentHistoryLatest.vue'
         }
         finally {
             loading.value = false;
+            if(environment.value?.environment_profile_id !== null) {
+                await loadActiveProfile();
+            }
+        }
+    }
+
+    async function loadActiveProfile() {
+        try {
+            const { data } = await EnvironmentProfileService.getEnvironmentProfile({ path: { id: environment.value?.environment_profile_id! } });
+            environmentProfile.value = data;
+        }
+        catch(err: any) {
+            //
+        }
+        finally {
+            // 
         }
     }
 
@@ -41,7 +58,12 @@ import EnvironmentHistoryLatest from '@/components/EnvironmentHistoryLatest.vue'
     <div v-else-if="error">{{ error }}</div>
     <div v-else>
         <div class="font-mono p-5">
-            {{ environment }}
+            <div>
+                {{ environment }}
+            </div>
+            <div>
+                {{ environmentProfile }}
+            </div>
         </div>
         <div>
             <EnvironmentHistoryLatest />
