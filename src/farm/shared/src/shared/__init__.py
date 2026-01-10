f8440bb8af7ca24d5d49b463fc5bc2138478ff6b
Shawn Renner <shawn.renner@gmail.com>
2026-01-09 23:37:06 -0600

    cleanup

--- src/farm/shared/src/shared/__init__.py
+++ src/farm/shared/src/shared/__init__.py
@@ -1,4 +1,4 @@
 from .models.base_table import BaseTable
 from .models.environment import Environment, EnvironmentCreate, EnvironmentPatch, EnvironmentPublic
-from .models.plant import Plant, PlantCreate, PlantPublic
 from .models.environment_profile import EnvironmentProfile, EnvironmentProfileCreate, EnvironmentProfilePatch, EnvironmentProfilePublic
+from .models.plant import Plant, PlantCreate, PlantPublic
