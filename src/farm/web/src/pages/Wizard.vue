140e373ebb7d54fe9f911a55ec143d91e81a2d11
Shawn Renner <shawn.renner@gmail.com>
2026-03-11 21:51:30 -0500

    stub wizard route

---
+++ src/farm/web/src/pages/Wizard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+    import { ref, onMounted } from 'vue';
+
+    const step = ref(1);
+
+    function nextStep() {
+        step.value++;
+    }
+
+    function prevStep() {
+        step.value--;
+    }
+</script>
+
+<template>
+    <h1>Wizard</h1>
+    <div>
+        <div v-if="step === 1">
+            <p>Step 1: Welcome to the wizard!</p>
+            <button @click="nextStep">Next</button>
+        </div>
+        <div v-else-if="step === 2">
+            <p>Step 2: This is the second step.</p>
+            <button @click="prevStep">Previous</button>
+            <button @click="nextStep">Next</button>
+        </div>
+        <div v-else-if="step === 3">
+            <p>Step 3: This is the final step.</p>
+            <button @click="prevStep">Previous</button>
+            <button @click="nextStep">Finish</button>
+        </div>
+        <div v-else-if="step === 4">
+            <p>Wizard completed! Thank you.</p>
+        </div>
+    </div>
+</template>
--- src/farm/web/src/router/index.ts
+++ src/farm/web/src/router/index.ts
@@ -1,5 +1,6 @@
 import EnvironmentDetail from '@/pages/EnvironmentDetail.vue'
 import Envrionments from '@/pages/Envrionments.vue'
+import Wizard from '@/pages/Wizard.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/environment/:id',
       name: 'Environment Detail',
       component: EnvironmentDetail
+    },
+    {
+      path: '/wizard',
+      name: 'Wizard',
+      component: Wizard
     }
   ],
 })
