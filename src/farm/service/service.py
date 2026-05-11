f3bfa34fb147745e5b365159ab47d99b81058feb
Shawn Renner <shawn.renner@gmail.com>
2026-05-10 20:05:28 -0500

    realistic mock data

--- src/farm/service/service.py
+++ src/farm/service/service.py
@@ -5,25 +5,32 @@ import queue
 from threading import Thread
 import requests
 import datetime
+import random
 
 # setup gpio pins, etc
 
 test_mode: bool = True
 
 def can_rx_task():
+    previous_temp = 22.5
+    previous_humidity = 50.0
     while True:
         if test_mode:
+            current_temp = previous_temp + random.uniform(-0.01, 0.01)
+            current_humidity = previous_humidity + random.uniform(-0.01, 0.01)
             payload: dict[str, str | int | float | bool] = {
                     "environment_id": 1,
                     "environment_profile_id": 1,
                     "light_status": True,
                     "heat_status": False,
-                    "temperature": 22.5,
-                    "humidity": 44.2,
+                    "temperature": current_temp,
+                    "humidity": current_humidity,
                     "gas": 0
             }
             response = requests.post('http://localhost:8000/live/environment-history', json=payload)
             print(response.text + ' ' + str(datetime.datetime.now()))
+            previous_temp = current_temp
+            previous_humidity = current_humidity
             time.sleep(.2)
 
 def can_tx_task():
